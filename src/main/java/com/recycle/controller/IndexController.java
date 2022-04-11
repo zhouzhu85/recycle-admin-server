4085fc4331df15806a98117b4b31cac7d05cd0e1
zhouzhu85 <734837014@qq.com>
2022-04-11 23:30:59 +0800

    新增报表图的分类数据

--- src/main/java/com/recycle/controller/IndexController.java
+++ src/main/java/com/recycle/controller/IndexController.java
@@ -1,19 +1,27 @@
 package com.recycle.controller;
 
 import com.recycle.model.RecycleResult;
+import com.recycle.model.TbCategory;
+import com.recycle.model.TbUsers;
+import com.recycle.server.CategoryService;
+import com.recycle.server.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UsersService usersService;
 
     @GetMapping("/")
     public ModelAndView index(ModelAndView modelAndView){
@@ -26,6 +34,11 @@ public class IndexController {
         modelAndView.setViewName("/page/welcome.html");
         return modelAndView;
     }
+
+    /**
+     * 报表图时间
+     * @return
+     */
     @GetMapping("chart_data")
     @ResponseBody
     public RecycleResult getChartData(){
@@ -44,6 +57,38 @@ public class IndexController {
         }
         thirtyDayList.remove(0);
         return RecycleResult.ok("查询成功",thirtyDayList);
+    }
+
+    /**
+     * 报表图的废品分类
+     */
+    @GetMapping("category_user_data")
+    @ResponseBody
+    public RecycleResult getCategoryData(){
+        Map resultMap=new HashMap<>();
+        List<TbCategory> categoryList = categoryService.findAll();
+        List<Map> newCategoryList = new ArrayList<>();
+        for (int i = 0; i < categoryList.size(); i++) {
+            TbCategory tbCategory = categoryList.get(i);
+            Map<String,Object> categoryMap=new HashMap<>();
+            categoryMap.put("name",tbCategory.getCategoryName());
+            categoryMap.put("value",tbCategory.getId());
+            if (i<=4){
+                categoryMap.put("selected",true);
+            }
+            newCategoryList.add(categoryMap);
+        }
+        resultMap.put("categoryData",newCategoryList);
+        List<TbUsers> usersList = usersService.findAllUsers();
+        List<Map> newUsersList=new ArrayList<>();
+        for (TbUsers tbUsers : usersList) {
+            Map<String,Object> userMap=new HashMap<>();
+            userMap.put("name",tbUsers.getUserName());
+            userMap.put("value",tbUsers.getId());
+            newUsersList.add(userMap);
+        }
+        resultMap.put("usersData",newUsersList);
+        return RecycleResult.ok("查询成功",resultMap);
     }
 
 }
